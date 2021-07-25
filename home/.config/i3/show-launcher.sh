f809456adcf13f181885c8da360b5382887d3fbe
noodlefighter <noodlefighter@gmail.com>
2021-07-25 22:34:48 +0800

    add rofi config to separated file

--- home/.config/i3/show-launcher.sh
+++ home/.config/i3/show-launcher.sh
@@ -18,8 +18,4 @@ if ! [ $? = 0 ]; then
    exit 0
 fi
 
-rofi -dpi 1 -show combi \
-   -combi-modi window,drun,run -width 30 -lines 10 -columns 1 \
-   -separator-style none -font "mono 12" -bw 0 \
-   -show-icons \
-   -kb-row-select "Tab" -kb-row-tab ""
+rofi -show combi -combi-modi window,drun,run 
